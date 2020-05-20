5d02315eba428e5bc12738576064e10c9fbca1b3
Ju <ju@thetreep.com>
2020-05-20 19:21:08 +0200

    add departure and arrival

--- segment.go
+++ segment.go
@@ -3,12 +3,13 @@ package covidtracker
 import "time"
 
 type Segment struct {
-	ID             SegID
-	Origin         string
-	Destination    string
-	DateTime       time.Time
-	Transportation Transportation
-	RiskLevel      float32
+	ID             SegID          `bson:"_id" json:"id"`
+	Origin         string         `bson:"origin" json:"origin"`
+	Destination    string         `bson:"destination" json:"destination"`
+	Departure      time.Time      `bson:"departure" json:"departure"`
+	Arrival        time.Time      `bson:"arrival" json:"arrival"`
+	Transportation Transportation `bson:"transportation" json:"transportation"`
+	RiskLevel      float32        `bson:"riskLevel" json:"riskLevel"`
 }
 
 type SegID string
