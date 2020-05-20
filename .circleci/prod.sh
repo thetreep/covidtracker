b9ca7a6d1a63c5ad75534326ead78e97e458641d
François-Xavier Aguessy <fx+dev@aguessy.fr>
2020-05-20 16:49:04 +0200

    fix CI

--- .circleci/prod.sh
+++ .circleci/prod.sh
@@ -5,4 +5,3 @@ docker push eu.gcr.io/the-treep-api-1525507752734/covidtracker
 
 # Replace deployment image
 kubectl set image deployment/covidtracker covidtracker=eu.gcr.io/the-treep-api-1525507752734/covidtracker:$CIRCLE_SHA1
-kubectl set image deployment/billing-cron billing-cron=eu.gcr.io/the-treep-api-1525507752734/billing-cron:$CIRCLE_SHA1
