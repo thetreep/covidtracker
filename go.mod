f864a6f54db17251a2eb8d76abc39c16f7ee73d2
Ju <ju@thetreep.com>
2020-05-20 13:14:32 +0200

    add indicator data

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/thetreep/covid-tracker
+module github.com/thetreep/covidtracker
 
 go 1.12
 
@@ -7,7 +7,7 @@ require (
 	github.com/graphql-go/graphql v0.7.9
 	github.com/graphql-go/handler v0.2.3
 	github.com/sirupsen/logrus v1.6.0
-	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/thetreep/toolbox v0.0.0-20200519135852-f98c1ce884cc // indirect
 	go.mongodb.org/mongo-driver v1.3.3
 )
--- job/datagouv/case.go
+++ job/datagouv/case.go
@@ -47,11 +47,11 @@ func (s *Service) RefreshCase() ([]*covidtracker.Case, error) {
 			continue
 		}
 		entry.HospServiceCountRelated, err = atoi(line[nb])
-		if s.handleParsingErr(err, "screening", "txPos") != nil {
+		if s.handleParsingErr(err, "covid_case", "txPos") != nil {
 			continue
 		}
 		entry.NoticeDate, err = time.Parse("2006-02-01", line[jour])
-		if s.handleParsingErr(err, "screening", "jour") != nil {
+		if s.handleParsingErr(err, "covid_case", "jour") != nil {
 			continue
 		}
 
