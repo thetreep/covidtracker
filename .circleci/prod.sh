adb97ec62104ddd3b989c490094eff1ea30778fc
François-Xavier Aguessy <fx+dev@aguessy.fr>
2020-06-10 17:29:47 +0200

    CD of refresher

--- .circleci/prod.sh
+++ .circleci/prod.sh
@@ -2,6 +2,8 @@
 
 # Push the new docker image
 docker push eu.gcr.io/the-treep-api-1525507752734/covidtracker
+docker push eu.gcr.io/the-treep-api-1525507752734/covidtracker-refresher
 
 # Replace deployment image
 kubectl set image deployment/covidtracker covidtracker=eu.gcr.io/the-treep-api-1525507752734/covidtracker:$CIRCLE_SHA1
+kubectl set image deployment/covidtracker-refresher covidtracker-refresher=eu.gcr.io/the-treep-api-1525507752734/covidtracker-refresher:$CIRCLE_SHA1
