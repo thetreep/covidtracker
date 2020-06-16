14f190f353f541273b5f4c280965d15b652e2880
François-Xavier Aguessy <fx+dev@aguessy.fr>
2020-06-16 16:35:14 +0200

    Add copyright

--- mock/risk.go
+++ mock/risk.go
@@ -1,3 +1,22 @@
+/*
+	This file is part of covidtracker also known as EviteCovid .
+
+    Copyright 2020 the Treep
+
+    covdtracker is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    covidtracker is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with covidtracker.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package mock
 
 import "github.com/thetreep/covidtracker"
